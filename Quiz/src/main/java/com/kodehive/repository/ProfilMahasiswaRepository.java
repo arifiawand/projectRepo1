b0bb4ee954146082a853e3c9c47f851d6d7ae72e
Fajar Ikhtiarwan <Fajar Ikhtiarwan@Fajarrwo>
2021-05-05 11:24:14 +0700

    update1

--- Quiz/src/main/java/com/kodehive/repository/ProfilMahasiswaRepository.java
+++ Quiz/src/main/java/com/kodehive/repository/ProfilMahasiswaRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.kodehive.model.MahasiswaModel;
 import com.kodehive.model.ProfilMahasiswaModel;
 
 public interface ProfilMahasiswaRepository extends JpaRepository<ProfilMahasiswaModel, Integer> {
 
-	
+	@Query("Select P from MahasiswaModel P where kdMahasiswa = ?1")
+	MahasiswaModel cariKodeMhs(String kode);
 }
